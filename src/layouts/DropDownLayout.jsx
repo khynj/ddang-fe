783fd534b21d3e95fb0a267c70c1d8e7e6346ec5
leejunghyeok <dkandjsl@gmail.com>
2025-02-03 14:53:04 +0900

    feature: Header feature

--- src/layouts/DropDownLayout.jsx
+++ src/layouts/DropDownLayout.jsx
@@ -4,12 +4,17 @@ import Navbar from '../components/navbar/Navbar'
 import { Outlet } from 'react-router-dom'
 import DropDownHeader from '../components/headers/DropDownHeader'
 
-function DropDownLayout({ back, routes }) {
+function DropDownLayout({ back, routes, feature }) {
   const [title, setTitle] = useState('...')
 
   return (
     <>
-      <DropDownHeader back={back} title={title} routes={routes} />
+      <DropDownHeader
+        back={back}
+        title={title}
+        routes={routes}
+        feature={feature}
+      />
       <Outlet context={{ setTitle }} />
       {back || <div className='py-8'></div>}
       {back || <Navbar />}
@@ -20,6 +25,7 @@ function DropDownLayout({ back, routes }) {
 DropDownLayout.propTypes = {
   back: PropTypes.bool,
   routes: PropTypes.array,
+  feature: PropTypes.object,
 }
 
 export default DropDownLayout
