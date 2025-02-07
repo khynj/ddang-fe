5e1ddc72161fce1d14d1ef6663620e89af064b1c
leejunghyeok <dkandjsl@gmail.com>
2025-02-07 15:47:54 +0900

    test: login API

--- src/features/user/pages/LoginPage.jsx
+++ src/features/user/pages/LoginPage.jsx
@@ -6,25 +6,48 @@ import googleIcon from '@/assets/images/oauth/google.png'
 import kakaoIcon from '@/assets/images/oauth/kakao.png'
 import naverIcon from '@/assets/images/oauth/naver.png'
 import StickyContainer from '@/components/StickyContainer'
+import ROUTES from '@/data/ROUTES'
+import { useLogin } from '@/apis'
+import { useState } from 'react'
 
 function LoginPage() {
   usePageName('로그인')
   const route = useNavigate()
+  const login = useLogin()
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const handleLogin = () => {
+    const res = login.mutate({
+      email,
+      password,
+    })
+    console.log(res)
+  }
   return (
     <div className='mt-2'>
       <div className=' p-4'>
-        <TextInput label='이메일' required type='email' />
-        <TextInput label='비밀번호' required type='password' />
+        <TextInput
+          label='이메일'
+          required
+          type='email'
+          value={email}
+          setValue={setEmail}
+        />
+        <TextInput
+          label='비밀번호'
+          required
+          type='password'
+          value={password}
+          setValue={setPassword}
+        />
         <div className='mt-8 mb-3'>
-          <DefaultButton onClick={() => route('/explore')}>
-            로그인
-          </DefaultButton>
+          <DefaultButton onClick={handleLogin}>로그인</DefaultButton>
         </div>
         <div className='px-1 p-2 flex flex-row justify-between text-gray-500 text-sm'>
-          <div className='flex flex-row gap-4'>
+          {/* <div className='flex flex-row gap-4'>
             <Link to='/popup/signup'>비밀번호 찾기</Link>
-          </div>
-          <Link to='/popup/signup'>회원가입</Link>
+          </div> */}
+          <Link to={ROUTES.SIGNUP}>회원가입</Link>
         </div>
       </div>
       <StickyContainer plain>
