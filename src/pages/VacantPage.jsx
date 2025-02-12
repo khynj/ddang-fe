c439917c593b3920811f25709cab2511a6200585
Hyeonji Kang <11qtrm@gmail.com>
2025-02-12 18:07:34 +0900

    feat: VacantPage

---
+++ src/pages/VacantPage.jsx
@@ -0,0 +1,12 @@
+export default function VancantPage() {
+  return (
+    <div className='flex flex-col items-center justify-center h-screen bg-gray-100 gap-8'>
+      <p className='text-gray-400 text-4xl font-bold mb-4'>텅 비어있어요</p>
+      <img
+        src='https://github.com/user-attachments/assets/8844d861-9c33-44e9-a961-f3be47455a23'
+        alt='mole'
+        className='w-30 h-auto'
+      />
+    </div>
+  )
+}
