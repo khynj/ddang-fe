f3a2847b7d787bfa10a75c4f94b2d5d098c0b9e5
leejunghyeok <dkandjsl@gmail.com>
2025-02-12 16:32:12 +0900

    feaet: filter bar css

--- src/components/FilterBar.jsx
+++ src/components/FilterBar.jsx
@@ -8,8 +8,8 @@ function FilterBar({ children }) {
   const { isOpen, open, close, value } = useModal('최신순')
   const options = ['최신순', '인기순', '마감임박']
   return (
-    <div className='flex px-4 py-2 justify-center items-center'>
-      <div className='flex gap-0 h-8'>{children}</div>
+    <div className='flex px-4 py-2 justify-between items-center'>
+      <div className='flex gap-2 w-full h-8'>{children}</div>
       <div
         className='font-bold text-sm flex gap-1 items-center text-gray-950'
         onClick={open}
