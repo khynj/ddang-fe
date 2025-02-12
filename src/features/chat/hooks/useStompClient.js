7159a480a29ffd138f2042dd7fb765597fc38421
leejunghyeok <dkandjsl@gmail.com>
2025-02-12 10:29:06 +0900

    feat: chatting

---
+++ src/features/chat/hooks/useStompClient.js
@@ -0,0 +1,58 @@
+import { useChatHistory } from '@/apis/chat'
+import { useAuth } from '@/contexts/AuthContext'
+import { useEffect, useState } from 'react'
+import Stomp from 'stompjs'
+
+const SOCKET_URL = `${import.meta.env.VITE_SERVER_URL}/api/ws-stomp` // Replace with your WebSocket URL
+
+export function useStompClient(id, scrollRef) {
+  const [stompClient, setStompClient] = useState(null)
+  const [messages, setMessages] = useState([])
+  const { data: chatHistory, isSuccess } = useChatHistory(id)
+  const { user } = useAuth()
+
+  const sendMessage = message => {
+    if (stompClient) {
+      stompClient.send(
+        `/pub/chat/message/${id}`,
+        {},
+        JSON.stringify({ memberId: user.memberId, content: message }),
+      )
+    }
+  }
+
+  useEffect(() => {
+    if (isSuccess) {
+      setMessages(chatHistory.chat)
+    }
+  }, [isSuccess, chatHistory])
+
+  useEffect(() => {
+    if (!isSuccess) return
+    const socket = new WebSocket(SOCKET_URL) // Use native WebSocket instead of SockJS
+    const stomp = Stomp.over(socket)
+
+    stomp.connect({}, () => {
+      console.log('Connected to STOMP server')
+
+      // Subscribe to chat room messages
+      const subscription = stomp.subscribe(`/sub/chat/room/${id}`, message => {
+        console.log(message)
+        setMessages(prev => [...prev, JSON.parse(message.body)])
+      })
+
+      setStompClient(stomp)
+
+      return () => {
+        subscription.unsubscribe() // Unsubscribe on cleanup
+        stomp.disconnect()
+      }
+    })
+  }, [id, isSuccess])
+
+  useEffect(() => {
+    scrollRef.current.scrollTop = scrollRef.current.scrollHeight
+  }, [messages, scrollRef])
+
+  return [messages, sendMessage]
+}
--- src/features/chat/pages/ChatroomPage.jsx
+++ src/features/chat/pages/ChatroomPage.jsx
@@ -1,63 +1,36 @@
-import { useEffect, useState } from 'react'
+import { useRef, useState } from 'react'
 import usePageName from '../../../hooks/usePageName'
 import ChatItem from '../components/ChatItem'
 import MaterialIcon from '@/components/icons/MaterialIcon'
 import { useParams } from 'react-router'
-import Stomp from 'stompjs'
-
-const SOCKET_URL = 'http://127.0.0.1:8080/ws-stomp' // Replace with your WebSocket URL
+import { useStompClient } from '../hooks/useStompClient'
 
 function ChatroomPage() {
   usePageName('채팅방')
 
-  const [stompClient, setStompClient] = useState(null)
-  const [messages, setMessages] = useState([])
-  const [content, setContent] = useState('')
+  const scrollRef = useRef(null)
   const chatRoomId = useParams().id
+  const [content, setContent] = useState('')
+  const [messages, sendMessage] = useStompClient(chatRoomId, scrollRef)
 
-  useEffect(() => {
-    const socket = new WebSocket(SOCKET_URL) // Use native WebSocket instead of SockJS
-    const stomp = Stomp.over(socket)
-
-    stomp.connect({}, () => {
-      console.log('Connected to STOMP server')
-
-      // Subscribe to chat room messages
-      const subscription = stomp.subscribe(
-        `/sub/chat/room/${chatRoomId}`,
-        message => {
-          console.log(message)
-          setMessages(prev => [...prev, JSON.parse(message.body)])
-        },
-      )
-
-      setStompClient(stomp)
-
-      return () => {
-        subscription.unsubscribe() // Unsubscribe on cleanup
-        stomp.disconnect()
-      }
-    })
-  }, [chatRoomId])
-
-  const sendMessage = () => {
-    if (stompClient) {
-      stompClient.send(
-        `/pub/chat/message/${chatRoomId}`,
-        {},
-        JSON.stringify({ memberId: 1, content }),
-      )
-    }
+  const onSubmit = e => {
+    e.preventDefault()
+    if (content.trim() === '') return
+    sendMessage(content)
+    setContent('')
   }
 
   return (
     <div className='flex flex-col justify-between h-[calc(100dvh-56px)]'>
-      <div className='overflow-y-scroll max-h-[calc(100dvh-56px-5rem)]'>
+      <div
+        className='max-w-svw overflow-x-hidden overflow-y-scroll max-h-[calc(100dvh-56px-5rem)]'
+        ref={scrollRef}
+      >
         {messages.map((chat, i) => {
           return <ChatItem key={i} chat={chat} />
         })}
       </div>
-      <div className='px-4 py-3 flex gap-2'>
+      <form onSubmit={onSubmit} className='px-4 py-3 flex gap-2'>
         <textarea
           type='text'
           className='w-full rounded-xl p-4 py-3 bg-gray-100'
@@ -68,11 +41,11 @@ function ChatroomPage() {
         />
         <button
           className='p-3 m-auto bg-gray-100 text-gray-700 rounded-xl aspect-square shrink-0 flex items-center justify-center'
-          onClick={sendMessage}
+          type='submit'
         >
           <MaterialIcon name='send' size={24} filled />
         </button>
-      </div>
+      </form>
     </div>
   )
 }
