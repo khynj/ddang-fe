522cf8c7d9bdd80bc98797e0eb87c2c6ed6e2866
고금강 <twinkay@yonsei.ac.kr>
2025-02-14 11:06:14 +0900

    카테고리 이미지 연결

--- src/features/product/components/category/CategoryList.jsx
+++ src/features/product/components/category/CategoryList.jsx
@@ -1,22 +1,31 @@
 import { useCategory } from '@/apis/auction'
 import PropTypes from 'prop-types'
-import bag from '@/assets/images/categories/bag.jpeg'
 import { Link } from 'react-router'
 import ROUTES from '@/data/ROUTES'
 
 function CategoryList({ category }) {
   const { data: subCategories } = useCategory(category.categoryId)
+
   return (
     <>
-      {subCategories?.map((subCategory, index) => (
+      {subCategories?.map((subCategory) => (
         <Link
           to={`${ROUTES.PRODUCT_LIST}?categoryId=${subCategory.categoryId}&categoryName=${subCategory.name}`}
-          key={index}
+          key={subCategory.categoryId}
           className='w-[30%]'
         >
-          <img src={bag} alt='' className='rounded-xl' />
-          {/* <img src={subCategory.imageUrl} alt='' className='rounded-xl' /> */}
-          <p className='text-center mt-1'>{subCategory.name}</p>
+          <div className="aspect-square overflow-hidden rounded-xl">
+            <img 
+              src={subCategory.imageUrl}
+              alt={subCategory.name}
+              className='w-full h-full object-cover'
+              onError={(e) => {
+                e.target.onerror = null
+                e.target.src = '/default-category-image.jpg' // 기본 이미지 경로
+              }}
+            />
+          </div>
+          <p className='text-center mt-2 text-sm'>{subCategory.name}</p>
         </Link>
       ))}
     </>
