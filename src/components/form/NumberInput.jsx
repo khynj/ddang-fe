a317949cd83daac22ec98baed20df668bf47558d
고금강 <twinkay@yonsei.ac.kr>
2025-02-14 15:36:04 +0900

    feat: 경매 시작가, 즉시 구매가 세자릿수 컴마 적용

--- src/components/form/NumberInput.jsx
+++ src/components/form/NumberInput.jsx
@@ -2,16 +2,21 @@ import PropTypes from 'prop-types'
 import Label from './Label'
 import { useState } from 'react'
 import InputError from './InputError'
+import { formatPrice } from '@/utils/formatPrice'
 
 function NumberInput({ label, required, value, setValue, validate }) {
   const [error, setError] = useState('')
   const onChange = e => {
-    const value = Number(e.target.value).toString()
+    // 숫자만 추출
+    const numericValue = e.target.value.replace(/[^0-9]/g, '')
+    const value = numericValue ? parseInt(numericValue) : 0
+    
     if (validate) {
       setError(validate(value))
     }
     setValue(value)
   }
+
   return (
     <div className='flex flex-col gap-2 py-3'>
       {label && (
@@ -20,8 +25,8 @@ function NumberInput({ label, required, value, setValue, validate }) {
         </Label>
       )}
       <input
-        type='number'
-        value={value}
+        type='text'
+        value={formatPrice(value)}
         onChange={onChange}
         className={`flex border-1 border-gray-300 rounded-md p-3 ${
           error ? 'invalid' : ''
