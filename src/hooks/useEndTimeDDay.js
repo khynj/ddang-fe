db307f550ba8d27d99f1054d6be0e3b9ddba9269
leejunghyeok <wndgur76@naver.com>
2025-02-16 23:35:58 +0900

    fix: related item cost

--- src/hooks/useEndTimeDDay.js
+++ src/hooks/useEndTimeDDay.js
@@ -5,8 +5,6 @@ export default function useEndTimeDDay(endTime) {
   const [currentTime, setCurrentTime] = useState(new Date().getTime())
 
   const endTimeDDay = useMemo(() => {
-    console.log(new Date(endTime).toString(), new Date(currentTime).toString())
-    console.log(new Date(endTime).toString() < new Date(currentTime).toString())
     if (new Date(endTime).getTime() < currentTime) return false
     return dday(endTime)
   }, [currentTime, endTime])
