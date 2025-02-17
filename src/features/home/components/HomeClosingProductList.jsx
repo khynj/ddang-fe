663c6b7514d94d58939e78e9dce0e63d6defeec0
고금강 <twinkay@yonsei.ac.kr>
2025-02-17 14:11:41 +0900

    feat: 마감 임박 페이지네이션 추가

--- src/features/home/components/HomeClosingProductList.jsx
+++ src/features/home/components/HomeClosingProductList.jsx
@@ -2,26 +2,33 @@ import { useSearchAuctions } from '@/apis/auction'
 import HomeListHeader from './HomeListHeader'
 import HomeProductItem from './HomeProductItem'
 import ROUTES from '@/data/ROUTES'
+import InfiniteScrollWrapper from '@/components/InfiniteScrollWrapper'
 
 function HomeClosingProductList() {
-  const { data: closingProducts } = useSearchAuctions({
+  const { data: closingProducts, fetchNextPage, isPending } = useSearchAuctions({
     sortType: 'endTime',
     size: 4,
   })
+
   return (
     <section>
       <HomeListHeader
         title={`마감 임박`}
         to={`${ROUTES.PRODUCT_LIST}?sortType=endTime`}
         icon='local_fire_department'
-      ></HomeListHeader>
-      <div className='grid grid-cols-2 gap-4'>
-        {closingProducts?.pages[0].auctionDetailProjection
-          .slice(0, 4)
-          .map((product, i) => (
-            <HomeProductItem key={i} product={product} />
-          ))}
-      </div>
+      />
+      <InfiniteScrollWrapper
+        fetchNextPage={fetchNextPage}
+        isPending={isPending}
+      >
+        <div className='grid grid-cols-2 gap-4'>
+          {closingProducts?.pages.map(page =>
+            page.auctionDetailProjection.map((product, i) => (
+              <HomeProductItem key={i} product={product} />
+            ))
+          )}
+        </div>
+      </InfiniteScrollWrapper>
     </section>
   )
 }
